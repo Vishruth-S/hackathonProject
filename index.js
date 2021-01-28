6b6b5df31cd1048dbce69c5691c30ff815ada97f
Vishruth S <vs.vishruth.s@gmail.com>
2021-01-28 12:55:20 +0530

    Added Auth

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const ejsMate = require('ejs-mate')
 const session = require('express-session')
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
-
-
+const userRoutes = require('./routes/users')
 const postRoutes = require('./routes/posts')
 const commentRoutes = require('./routes/comments')
 
@@ -44,12 +46,20 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
+    res.locals.currentUser = req.user
     next()
 })
 
@@ -58,6 +68,7 @@ app.get('/', (req, res) => {
 })
 
 
+app.use('/', userRoutes)
 app.use('/posts/', postRoutes)
 app.use('/posts/:id/comments/', commentRoutes)
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
+const { isLoggedIn } = require('../middleware')
 
 router.get('/', async (req, res) => {
     const posts = await Post.find({})
     res.render('posts/index', { posts })
 })
 
-router.get('/new', (req, res) => {
-    res.render("posts/new")
+router.get('/new', isLoggedIn, (req, res) => {
+    res.render('posts/new')
 })
 
-router.post('/', async (req, res) => {
+router.post('/', isLoggedIn, async (req, res) => {
     const post = new Post(req.body.post)
     await post.save()
     req.flash('success', 'Sucessfully made a new Post!')
@@ -27,7 +28,7 @@ router.get('/:id', async (req, res) => {
     res.render('posts/show', { post })
 })
 
-router.get('/:id/edit', async (req, res) => {
+router.get('/:id/edit', isLoggedIn, async (req, res) => {
     const post = await Post.findById(req.params.id)
     if (!post) {
         req.flash('error', 'Cannot find that post')
@@ -36,14 +37,14 @@ router.get('/:id/edit', async (req, res) => {
     res.render('posts/edit', { post })
 })
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', isLoggedIn, async (req, res) => {
     const { id } = req.params
     const post = await Post.findByIdAndUpdate(id, { ...req.body.post })
     req.flash('success', 'Sucessfully updated!')
     res.redirect(`/posts/${post._id}`)
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', isLoggedIn, async (req, res) => {
     const { id } = req.params
     await Post.findByIdAndDelete(id)
     req.flash('success', 'Post was deleted successfully')
