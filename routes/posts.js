410878169a1bc867cd454798e891b336540cd1e0
Vishruth Subramanian <vs.vishruth.s@gmail.com>
2022-10-31 22:00:13 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -14,27 +14,6 @@ router.get('/new', isLoggedIn, (req, res) => {
     res.render('posts/new')
 })
 
-router.post('/', isLoggedIn, async (req, res) => {
-    const post = new Post(req.body.post)
-    post.author = req.user._id
-    await post.save()
-    req.flash('success', 'Sucessfully made a new Post!')
-    res.redirect(`/posts/${post._id}`)
-})
-
-router.get('/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id).populate({
-        path: 'comments',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author')
-    if (!post) {
-        req.flash('error', 'Cannot find that post')
-        return res.redirect('/posts/')
-    }
-    res.render('posts/show', { post })
-})
 
 router.get('/:id/edit', isLoggedIn, isAuthor, async (req, res) => {
     const post = await Post.findById(req.params.id)
